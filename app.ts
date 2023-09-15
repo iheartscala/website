34e22e9e7d15a48bce6585322fca1a5d59aa07b9
SunKing2 <louievbl4t@gmail.com>
2023-09-15 13:59:52 -0400

    added a basic ts file for deno

--- app.ts
+++ app.ts
@@ -1,1 +1,1 @@
-console.log("hi there, I'm the app.ts file needed for deno.  I'm probably deploying now :)")
+console.log("Hi there, I'm the app.ts file needed for deno.  I'm probably deploying now :)")
